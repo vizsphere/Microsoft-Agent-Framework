87eb662fa187432e51f9ab44f6c072aee5b1879c
pervaiza <m.akhtar@hotmail.co.uk>
2025-11-25 20:38:54 +0000

    removed

--- Agent_Framework_Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Agent_Framework_Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Agent_Framework_Web/Models/PromptModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Agent_Framework_Web.Models
-{
-    public class PromptModel
-    {
-        public string Prompt { get; set; }
-
-        public string Response { get; set; }
-    }
-}
